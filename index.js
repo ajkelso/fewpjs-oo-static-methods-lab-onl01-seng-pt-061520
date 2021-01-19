45581644ca8d9f4c33ecf01f9fe0383ab30b93f5
ajkelso <ajkelso@gmail.com>
2021-01-19 15:54:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.capitalize()
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string) {
+    let strArray = string.capitalize.split(" ")
+    let badWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from" ]
+    let titleArr = strArray.map(word => {
+      if (!badWords.includes(word)) {
+        return word.capitalize()
+      } else {
+        return word
+      }
+    })
+    return titleArr.join(" ")
+  }
 }
